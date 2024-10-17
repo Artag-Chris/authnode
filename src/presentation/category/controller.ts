d82c20a7848d090d989f16f31b7ba3f78a6daf42
Artag-Chris <scristxyz@gmail.com>
2024-10-17 16:59:04 -0500

    act

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,8 +1,11 @@
 import { CreateCategoryDto, CustomError } from "../../domain";
 import { Request, Response } from "express";
+import { CategoryService } from "../services/categoryService";
 
 export class CategoryController {
-  constructor() {}
+  constructor(
+    private readonly categoryService: CategoryService
+  ) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError) {
@@ -12,10 +15,14 @@ export class CategoryController {
     return res.status(500).json({ error: `Internal Server Error` });
   };
 
-  createCategory = async (req: Request, res: Response) => {
+  createCategory = (req: Request, res: Response) => {
     const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
     if (error) return res.status(400).json({ error });
-    res.json(createCategoryDto);
+   this.categoryService
+      .createCategory(createCategoryDto!, req.body.user)
+      .then((category) => res.json(category))
+      .catch((error) => this.handleError(error, res));
+
   };
 
   getCategories = async (req: Request, res: Response) => {
