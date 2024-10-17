3a8544943a4ece0d0aa96782d3d1504885dd436e
Artag-Chris <scristxyz@gmail.com>
2024-10-17 15:42:26 -0500

    act

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,5 @@
 export * from './errors/customError';
 export * from './dtos/auth/registerUser.dto';
 export * from './entities/userEntity';
+export * from './dtos/auth/loginUser.dto';
+export * from './dtos/category/createCategory.dto';
