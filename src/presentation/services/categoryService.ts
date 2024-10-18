9f141c4479abfc71141c78766f08e25274c0bca4
Artag-Chris <scristxyz@gmail.com>
2024-10-18 10:22:52 -0500

    pagination

--- src/presentation/services/categoryService.ts
+++ src/presentation/services/categoryService.ts
@@ -33,16 +33,31 @@ async getCategories(paginationDto:PaginationDTO) {
     const {page,limit}=paginationDto;
 
     try {
-        
+    
+        const[total,categories] = await Promise.all([
+            CategoryModel.countDocuments(),
+            CategoryModel.find()
+            .skip((page-1)*limit)
+            .limit(limit)
+        ])
+
+   /* const total = await CategoryModel.countDocuments();    
     const categories = await CategoryModel.find()
     .skip((page-1)*limit)
-    .limit(limit);
-    
-    return categories.map((category) => ({
-        id: category.id,
-        name: category.name,
-        available: category.available
-    }))
+    .limit(limit);*/
+
+    return {
+        page,
+        limit,
+        total,
+        next:`/api/categories?page=${page+1}&limit=${limit}`,
+        prev:(page -1>0)? `/api/categories?page=${page-1}&limit=${limit}`:null,
+        categories:categories.map((category) => ({
+            id: category.id,
+            name: category.name,
+            available: category.available
+        }))
+    }
 
     } catch (error) {
         throw CustomError.internalServer(`${error}`);
