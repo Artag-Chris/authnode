a368adfacdafe06e7b956eb318afc4618164c6ac
Artag-Chris <scristxyz@gmail.com>
2024-10-21 10:28:22 -0500

    act

--- src/presentation/services/productService.ts
+++ src/presentation/services/productService.ts
@@ -1,63 +1,75 @@
-import { ProductModel } from "../../data";
-import { CreateProductDto, CustomError, PaginationDTO, UserEntity } from "../../domain";
+import { ProductModel } from '../../data';
+import { CreateProductDto, CustomError, PaginationDTO } from '../../domain';
+
 
 
 export class ProductService {
 
-constructor() {}
-
-async createProduct(createProductDto: CreateProductDto) {
-
-const productExist = await ProductModel.findOne({ name: createProductDto.name });
-if (productExist) throw CustomError.badRequest('product already exist');
-
-try {
+  // DI
+  constructor() { }
 
 
-    const product = new ProductModel({ ...createProductDto });
-    await product.save();
-    return product
-    
-} catch (error) {
-    throw CustomError.internalServer(`${error}`);
-}
+  async createProduct( createProductDto: CreateProductDto ) {
 
-}
-
-async getProducts(paginationDto:PaginationDTO) {
-
-    const {page,limit}=paginationDto;
+    const productExists = await ProductModel.findOne( { name: createProductDto.name } );
+    if ( productExists ) throw CustomError.badRequest( 'Product already exists' );
 
     try {
-    
-        const[total,products] = await Promise.all([
-            ProductModel.countDocuments(),
-            ProductModel.find()
-            .skip((page-1)*limit)
-            .limit(limit)
-        ])
 
-   /* const total = await CategoryModel.countDocuments();    
-    const categories = await CategoryModel.find()
-    .skip((page-1)*limit)
-    .limit(limit);*/
+      const product = new ProductModel( createProductDto );
 
-    return {
-        page,
-        limit,
-        total,
-        next:`/api/produtcs?page=${page+1}&limit=${limit}`,
-        prev:(page -1>0)? `/api/produtcs?page=${page-1}&limit=${limit}`:null,
-        products:products.map((product) => ({
-            id: product.id,
-            name: product.name,
-            available: product.available
-        }))
+      await product.save();
+
+      return product;
+
+    } catch ( error ) {
+      throw CustomError.internalServer( `${ error }` );
     }
 
-    } catch (error) {
-        throw CustomError.internalServer(`${error}`);
+  }
+
+
+
+  async getProducts( paginationDto: PaginationDTO ) {
+
+    const { page, limit } = paginationDto;
+
+
+    try {
+
+      const [ total, products ] = await Promise.all( [
+        ProductModel.countDocuments(),
+        ProductModel.find()
+          .skip( ( page - 1 ) * limit )
+          .limit( limit )
+          .populate('user')
+          .populate('category')
+          // .populate('user', 'name email')
+      ] );
+
+
+      return {
+        page: page,
+        limit: limit,
+        total: total,
+        next: `/api/products?page=${ ( page + 1 ) }&limit=${ limit }`,
+        prev: ( page - 1 > 0 ) ? `/api/products?page=${ ( page - 1 ) }&limit=${ limit }` : null,
+
+        products: products,
+      };
+
+
+    } catch ( error ) {
+      throw CustomError.internalServer( 'Internal Server Error' );
     }
-}
+
+
+
+
+  }
+
+
+
 
 }
+
