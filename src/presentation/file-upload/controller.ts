e53e2c83630c61657c520b6d003b2688ca92609a
Artag-Chris <scristxyz@gmail.com>
2024-10-24 16:39:37 -0500

    act

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -20,6 +20,11 @@ export class FileUploadController {
   uploadFile = (req: Request, res: Response) => {
     
     const files = req.files;
+    const type = req.params.type;
+    const validTypes = ["users", "products", "categories"];
+    if (!validTypes.includes(type)) {
+      return res.status(400).json({ error: `Invalid type : ${ type }, valid types: ${validTypes}` });
+    }
 
     if(!req.files||Object.keys(req.files).length===0){
       return res.status(400).json({error:'No files were uploaded'});
@@ -29,7 +34,7 @@ export class FileUploadController {
     const file = files!["undefined"] as UploadedFile;
    
     this.fileUploadService
-      .uploadSingle(file)
+      .uploadSingle(file, `uploads/${ type }`)
       .then((uploaded) => res.json(uploaded))
       .catch((error) => this.handleError(error, res));
 
